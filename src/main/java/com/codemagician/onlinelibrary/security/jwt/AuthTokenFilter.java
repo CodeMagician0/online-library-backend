bc2bba97eb57fb4f0269a614929167dfd6efa0b9
SiuyunYip <yzyyezhaoyuan@gmail.com>
2023-09-08 09:41:29 +0100

    bug fix: authTokenFilter is called twice

--- src/main/java/com/codemagician/onlinelibrary/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/codemagician/onlinelibrary/security/jwt/AuthTokenFilter.java
@@ -12,7 +12,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -26,7 +25,6 @@ import java.io.IOException;
  * Filter applied to each request with the implementation of parsing & validating JWT
  */
 
-@Component
 public class AuthTokenFilter extends OncePerRequestFilter {
 
     @Autowired
